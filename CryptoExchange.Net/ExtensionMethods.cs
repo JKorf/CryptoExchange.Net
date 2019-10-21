0699d91b04b69acc2271a40297ed9a57e5a2df2d
JKorf <jankorf91@gmail.com>
2019-10-21 16:36:31 +0200

    Added validation methods

--- CryptoExchange.Net/ExtensionMethods.cs
+++ CryptoExchange.Net/ExtensionMethods.cs
@@ -213,5 +213,55 @@ namespace CryptoExchange.Net
                 return null;
             }
         }
+
+        /// <summary>
+        /// Validates an int is one of the allowed values
+        /// </summary>
+        /// <param name="value">Value of the int</param>
+        /// <param name="argumentName">Name of the parameter</param>
+        /// <param name="allowedValues">Allowed values</param>
+        public static void ValidateIntValues(this int value, string argumentName, params int[] allowedValues)
+        {
+            if (!allowedValues.Contains(value))
+                throw new ArgumentException(
+                    $"{value} not allowed for parameter {argumentName}, allowed values: {string.Join(", ", allowedValues)}");
+        }
+
+        /// <summary>
+        /// Validates an int is between two values
+        /// </summary>
+        /// <param name="value">The value of the int</param>
+        /// <param name="argumentName">Name of the parameter</param>
+        /// <param name="minValue">Min value</param>
+        /// <param name="maxValue">Max value</param>
+        public static void ValidateIntBetween(this int value, string argumentName, int minValue, int maxValue)
+        {
+            if (value < minValue || value > maxValue)
+                throw new ArgumentException(
+                    $"{value} not allowed for parameter {argumentName}, min: {minValue}, max: {maxValue}");
+        }
+
+        /// <summary>
+        /// Validates a string is not null or empty
+        /// </summary>
+        /// <param name="value">The value of the string</param>
+        /// <param name="argumentName">Name of the parameter</param>
+        public static void ValidateNotNull(this string value, string argumentName)
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentException($"No value provided for parameter {argumentName}");
+        }
+
+        /// <summary>
+        /// Validates an object is not null
+        /// </summary>
+        /// <param name="value">The value of the object</param>
+        /// <param name="argumentName">Name of the parameter</param>
+        public static void ValidateNotNull(this object value, string argumentName)
+        {
+            if (value == null)
+                throw new ArgumentException($"No value provided for parameter {argumentName}");
+        }
     }
 }
+
