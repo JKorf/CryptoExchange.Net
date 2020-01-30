08d7022815200aad6a6a32642c5af84417bd976d
Ben Davison <ben@boardfresh.com>
2020-01-30 16:55:47 +0000

    introduce a default empty ISymbolOrderBookEntry that is returned buy BestBid or BestAsk if called when the bid or ask lists are empty.  This resolves a null reference exception seen during syncronization (specifically when connecting to Kraken).  I have also introduced BestOffers which returns both bid and ask in the scope of one lock allowing the caller to be sure that nothing has changed between BestBid and BestAsk request.

---
+++ CryptoExchange.Net.UnitTests/SymbolOrderBookTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using CryptoExchange.Net.Logging;
+using CryptoExchange.Net.Objects;
+using CryptoExchange.Net.OrderBook;
+using CryptoExchange.Net.Sockets;
+using CryptoExchange.Net.UnitTests.TestImplementations;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace CryptoExchange.Net.UnitTests
+{
+    [TestFixture]
+    public class SymbolOrderBookTests
+    {
+        private static OrderBookOptions defaultOrderBookOptions = new OrderBookOptions("Test", true);
+
+        private class TestableSymbolOrderBook : SymbolOrderBook
+        {
+            public TestableSymbolOrderBook() : base("BTC/USD", defaultOrderBookOptions)
+            {
+            }
+
+            public override void Dispose()
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override Task<CallResult<bool>> DoResync()
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override Task<CallResult<UpdateSubscription>> DoStart()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [TestCase]
+        public void GivenEmptyBidList_WhenBestBid_ThenEmptySymbolOrderBookEntry()
+        {
+            var symbolOrderBook = new TestableSymbolOrderBook();
+            Assert.IsNotNull(symbolOrderBook.BestBid);
+            Assert.AreEqual(0m, symbolOrderBook.BestBid.Price);
+            Assert.AreEqual(0m, symbolOrderBook.BestAsk.Quantity);
+        }
+
+        [TestCase]
+        public void GivenEmptyAskList_WhenBestAsk_ThenEmptySymbolOrderBookEntry()
+        {
+            var symbolOrderBook = new TestableSymbolOrderBook();
+            Assert.IsNotNull(symbolOrderBook.BestBid);
+            Assert.AreEqual(0m, symbolOrderBook.BestBid.Price);
+            Assert.AreEqual(0m, symbolOrderBook.BestAsk.Quantity);
+        }
+
+        [TestCase]
+        public void GivenEmptyBidAndAskList_WhenBestOffers_ThenEmptySymbolOrderBookEntries()
+        {
+            var symbolOrderBook = new TestableSymbolOrderBook();
+            Assert.IsNotNull(symbolOrderBook.BestOffers);
+            Assert.IsNotNull(symbolOrderBook.BestOffers.Item1);
+            Assert.IsNotNull(symbolOrderBook.BestOffers.Item2);
+            Assert.AreEqual(0m, symbolOrderBook.BestOffers.Item1.Price);
+            Assert.AreEqual(0m, symbolOrderBook.BestOffers.Item1.Quantity);
+            Assert.AreEqual(0m, symbolOrderBook.BestOffers.Item2.Price);
+            Assert.AreEqual(0m, symbolOrderBook.BestOffers.Item2.Quantity);
+        }
+    }
+}
