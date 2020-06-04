c60131d46407371eee2bf8963fd131ab81060fbc
Jan Korf <jankorf91@gmail.com>
2020-06-04 22:01:42 +0200

    wip orderbook rework

---
+++ CryptoExchange.Net/OrderBook/ProcessQueueItem.cs
@@ -0,0 +1,13 @@
+﻿using CryptoExchange.Net.Interfaces;
+using System.Collections.Generic;
+
+namespace CryptoExchange.Net.OrderBook
+{
+    internal class ProcessQueueItem
+    {
+        public long StartUpdateId { get; set; }
+        public long EndUpdateId { get; set; }
+        public IEnumerable<ISymbolOrderBookEntry> Bids { get; set; } = new List<ISymbolOrderBookEntry>();
+        public IEnumerable<ISymbolOrderBookEntry> Asks { get; set; } = new List<ISymbolOrderBookEntry>();
+    }
+}
