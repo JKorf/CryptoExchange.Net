6b4585993450daba95b84292fdc0d73f0f9dc7b7
Jkorf <jankorf91@gmail.com>
2022-01-03 14:08:35 +0100

    Updated example

--- Examples/BlazorClient/Program.cs
+++ Examples/BlazorClient/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace BlazorClient
 {
@@ -7,6 +10,13 @@ namespace BlazorClient
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Error)
+                .WriteTo.File("log-.txt", rollingInterval: RollingInterval.Day, flushToDiskInterval: System.TimeSpan.FromSeconds(1))
+                .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -14,7 +24,8 @@ namespace BlazorClient
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup(
+                        context => new Startup(context.Configuration, LoggerFactory.Create(config => config.AddSerilog()) ));
                 });
     }
 }
--- Examples/BlazorClient/Startup.cs
+++ Examples/BlazorClient/Startup.cs
@@ -1,31 +1,19 @@
+using System.Collections.Generic;
 using Binance.Net;
 using Binance.Net.Clients;
 using Binance.Net.Interfaces.Clients;
 using Binance.Net.Objects;
 using Bitfinex.Net;
-using Bitfinex.Net.Clients;
-using Bitfinex.Net.Interfaces.Clients;
 using Bittrex.Net;
-using Bittrex.Net.Clients;
-using Bittrex.Net.Interfaces.Clients;
 using Bybit.Net;
-using Bybit.Net.Clients;
-using Bybit.Net.Interfaces.Clients;
+using CoinEx.Net;
 using CoinEx.Net.Clients;
 using CoinEx.Net.Interfaces.Clients;
 using CryptoExchange.Net.Authentication;
 using FTX.Net;
-using FTX.Net.Clients;
-using FTX.Net.Interfaces.Clients;
 using Huobi.Net;
-using Huobi.Net.Clients;
-using Huobi.Net.Interfaces.Clients;
 using Kraken.Net;
-using Kraken.Net.Clients;
-using Kraken.Net.Interfaces.Clients;
 using Kucoin.Net;
-using Kucoin.Net.Clients;
-using Kucoin.Net.Interfaces.Clients;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -37,9 +25,12 @@ namespace BlazorClient
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private ILoggerFactory _loggerFactory;
+
+        public Startup(IConfiguration configuration, ILoggerFactory loggerFactory)
         {
             Configuration = configuration;
+            _loggerFactory = loggerFactory;
         }
 
         public IConfiguration Configuration { get; }
@@ -54,22 +45,36 @@ namespace BlazorClient
             // Register the clients, options can be provided in the callback parameter
             services.AddBinance((restClientOptions, socketClientOptions) => {
                 restClientOptions.ApiCredentials = new ApiCredentials("KEY", "SECRET");
-                restClientOptions.LogLevel = LogLevel.Debug;
+                restClientOptions.LogLevel = LogLevel.Trace;
+
+                // Point the logging to use the ILogger configuration, which uses Serilog here
+                restClientOptions.LogWriters = new List<ILogger> { _loggerFactory.CreateLogger<IBinanceClient>() };
 
                 socketClientOptions.ApiCredentials = new ApiCredentials("KEY", "SECRET");
             });
+
+            BinanceClient.SetDefaultOptions(new BinanceClientOptions
+            {
+                ApiCredentials = new ApiCredentials("KEY", "SECRET"),
+                LogLevel = LogLevel.Trace
+            });
+
+            BinanceSocketClient.SetDefaultOptions(new BinanceSocketClientOptions
+            {
+                ApiCredentials = new ApiCredentials("KEY", "SECRET"),
+            });
+
+            services.AddTransient<IBinanceClient, BinanceClient>();
+            services.AddScoped<IBinanceSocketClient, BinanceSocketClient>();
+
             services.AddBitfinex();
             services.AddBittrex();
             services.AddBybit();
-            //services.AddCoinEx();
+            services.AddCoinEx();
             services.AddFTX();
             services.AddHuobi();
             services.AddKraken();
             services.AddKucoin();
-
-            // TODO remove when AddCoinEx fixed
-            services.AddScoped<ICoinExClient, CoinExClient>();
-            services.AddScoped<ICoinExSocketClient, CoinExSocketClient>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
