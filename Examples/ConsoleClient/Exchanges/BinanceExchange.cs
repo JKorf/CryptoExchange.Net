fe31cf156d76c41159ff4ffee02c720929a2aaa4
Jkorf <jankorf91@gmail.com>
2022-01-19 16:35:08 +0100

    Examples

---
+++ Examples/ConsoleClient/Exchanges/BinanceExchange.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Binance.Net.Clients;
+
+namespace ConsoleClient.Exchanges
+{
+    internal class BinanceExchange : IExchange
+    {
+        public async Task<decimal> GetPrice(string symbol)
+        {
+            using var client = new BinanceClient();
+            var result = await client.SpotApi.ExchangeData.GetPriceAsync(symbol);
+            // Should check result success status here
+            return result.Data.Price;
+        }
+    }
+}
---
+++ Examples/ConsoleClient/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ConsoleClient.Exchanges;
+
+namespace ConsoleClient
+{
+    internal class Program
+    {
+        static Dictionary<string, IExchange> _exchanges = new Dictionary<string, IExchange>
+        {
+            { "Binance", new BinanceExchange() }
+        };
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("> Available commands: PlaceOrder, GetOrders, GetPrice");
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "PlaceOrder":
+
+                        break;
+                    case "GetOrders":
+
+                        break;
+                    case "GetPrice":
+                        await ProcessGetPrice();
+                        break;
+                    default:
+                        Console.WriteLine("> Unknown command");
+                        break;
+                }
+            }
+        }
+
+        static async Task ProcessGetPrice()
+        {
+            Console.WriteLine("> Exchange?");
+            var exchange = Console.ReadLine();
+
+            Console.WriteLine("> Symbol?");
+            var symbol = Console.ReadLine();
+
+            var price = await _exchanges[exchange].GetPrice(symbol);
+            Console.WriteLine($"> {exchange} price for {symbol}: {price}");
+        }
+    }
+}
