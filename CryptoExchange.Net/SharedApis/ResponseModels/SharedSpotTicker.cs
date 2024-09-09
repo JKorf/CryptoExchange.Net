788712e36f38489215515334d30fe0feb29975a3
Jkorf <jankorf91@gmail.com>
2024-09-09 15:34:32 +0200

    wip

--- CryptoExchange.Net/SharedApis/ResponseModels/SharedSpotTicker.cs
+++ CryptoExchange.Net/SharedApis/ResponseModels/SharedSpotTicker.cs
@@ -7,12 +7,12 @@ namespace CryptoExchange.Net.SharedApis.RequestModels
     public record SharedSpotTicker
     {
         public string Symbol { get; set; }
-        public decimal LastPrice { get; set; }
-        public decimal HighPrice { get; set; }
-        public decimal LowPrice { get; set; }
+        public decimal? LastPrice { get; set; }
+        public decimal? HighPrice { get; set; }
+        public decimal? LowPrice { get; set; }
         public decimal Volume { get; set; }
 
-        public SharedSpotTicker(string symbol, decimal lastPrice, decimal highPrice, decimal lowPrice, decimal volume)
+        public SharedSpotTicker(string symbol, decimal? lastPrice, decimal? highPrice, decimal? lowPrice, decimal volume)
         {
             Symbol = symbol;
             LastPrice = lastPrice;
