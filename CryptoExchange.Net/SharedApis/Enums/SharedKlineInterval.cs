024e8dcfe2325e6bfbb4f5aaba83e479dd604380
JKorf <jankorf91@gmail.com>
2025-02-09 20:12:55 +0100

    Added SharedKlineInterval values

--- CryptoExchange.Net/SharedApis/Enums/SharedKlineInterval.cs
+++ CryptoExchange.Net/SharedApis/Enums/SharedKlineInterval.cs
@@ -6,6 +6,14 @@
     public enum SharedKlineInterval
     {
         /// <summary>
+        /// 1 min
+        /// </summary>
+        OneMinute = 60,
+        /// <summary>
+        /// 3 min
+        /// </summary>
+        ThreeMinutes = 60 * 3,
+        /// <summary>
         /// 5 min
         /// </summary>
         FiveMinutes = 60 * 5,
@@ -14,9 +22,33 @@
         /// </summary>
         FifteenMinutes = 60 * 15,
         /// <summary>
+        /// Thirty minutes
+        /// </summary>
+        ThirtyMinutes = 60 * 30,
+        /// <summary>
         /// 1 hour
         /// </summary>
         OneHour = 60 * 60,
+        /// <summary>
+        /// 2 hours
+        /// </summary>
+        TwoHours = 60 * 60 * 2,
+        /// <summary>
+        /// 4 hours
+        /// </summary>
+        FourHours = 60 * 60 * 4,
+        /// <summary>
+        /// 6 hours
+        /// </summary>
+        SixHours = 60 * 60 * 6,
+        /// <summary>
+        /// 8 hours
+        /// </summary>
+        EightHours = 60 * 60 * 8,
+        /// <summary>
+        /// 12 hours
+        /// </summary>
+        TwelveHours = 60 * 60 * 12,
         /// <summary>
         /// 1 day
         /// </summary>
