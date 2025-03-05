2f6889880a2511ba6c510a20a4ab459079d196b1
Jkorf <jankorf91@gmail.com>
2025-03-05 16:11:09 +0100

    Restore CryptoRestClient

---
+++ CryptoExchange.Net/Clients/CryptoRestClient.cs
@@ -0,0 +1,27 @@
+﻿﻿using CryptoExchange.Net.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CryptoExchange.Net.Clients
+{
+    /// <inheritdoc />
+    public class CryptoRestClient : CryptoBaseClient, ICryptoRestClient
+    {
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public CryptoRestClient()
+        {
+        }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        public CryptoRestClient(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+    }
+}
