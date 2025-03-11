932cc4864e77dfc39feeb1d26485d50ee1f63b1c
Jkorf <jankorf91@gmail.com>
2025-03-11 15:15:43 +0100

    Add ClientOrderId shared apis

---
+++ CryptoExchange.Net/SharedApis/Interfaces/Rest/Futures/IFuturesOrderClientIdClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CryptoExchange.Net.SharedApis
+{
+    /// <summary>
+    /// Client for managing futures orders using a client order id
+    /// </summary>
+    public interface IFuturesOrderClientIdClient : ISharedClient
+    {
+        /// <summary>
+        /// Futures get order by client order id request options
+        /// </summary>
+        EndpointOptions<GetOrderRequest> GetFuturesOrderByClientOrderIdOptions { get; }
+
+        /// <summary>
+        /// Get info on a specific futures order using a client order id
+        /// </summary>
+        /// <param name="request">Request info</param>
+        /// <param name="ct">Cancellation token</param>
+        Task<ExchangeWebResult<SharedFuturesOrder>> GetFuturesOrderByClientOrderIdAsync(GetOrderRequest request, CancellationToken ct = default);
+
+        /// <summary>
+        /// Futures cancel order by client order id request options
+        /// </summary>
+        EndpointOptions<CancelOrderRequest> CancelFuturesOrderByClientOrderIdOptions { get; }
+        /// <summary>
+        /// Cancel a futures order using client order id
+        /// </summary>
+        /// <param name="request">Request info</param>
+        /// <param name="ct">Cancellation token</param>
+        Task<ExchangeWebResult<SharedId>> CancelFuturesOrderByClientOrderIdAsync(CancelOrderRequest request, CancellationToken ct = default);
+    }
+}
---
+++ CryptoExchange.Net/SharedApis/Interfaces/Rest/Spot/ISpotOrderClientIdClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CryptoExchange.Net.SharedApis
+{
+    /// <summary>
+    /// Client for managing spot orders using a client order id
+    /// </summary>
+    public interface ISpotOrderClientIdClient : ISharedClient
+    {
+        /// <summary>
+        /// Spot get order by client order id request options
+        /// </summary>
+        EndpointOptions<GetOrderRequest> GetSpotOrderByClientOrderIdOptions { get; }
+
+        /// <summary>
+        /// Get info on a specific spot order using a client order id
+        /// </summary>
+        /// <param name="request">Request info</param>
+        /// <param name="ct">Cancellation token</param>
+        Task<ExchangeWebResult<SharedSpotOrder>> GetSpotOrderByClientOrderIdAsync(GetOrderRequest request, CancellationToken ct = default);
+
+        /// <summary>
+        /// Spot cancel order by client order id request options
+        /// </summary>
+        EndpointOptions<CancelOrderRequest> CancelSpotOrderByClientOrderIdOptions { get; }
+        /// <summary>
+        /// Cancel a spot order using client order id
+        /// </summary>
+        /// <param name="request">Request info</param>
+        /// <param name="ct">Cancellation token</param>
+        Task<ExchangeWebResult<SharedId>> CancelSpotOrderByClientOrderIdAsync(CancelOrderRequest request, CancellationToken ct = default);
+    }
+}
