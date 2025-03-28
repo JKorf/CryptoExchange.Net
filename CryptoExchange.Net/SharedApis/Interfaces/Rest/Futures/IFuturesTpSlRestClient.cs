885c37a25ead038934a99be64a91c90133b565eb
Jkorf <jankorf91@gmail.com>
2025-03-28 10:34:04 +0100

    wip

---
+++ CryptoExchange.Net/SharedApis/Interfaces/Rest/Futures/IFuturesTpSlRestClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CryptoExchange.Net.SharedApis
+{
+    public interface IFuturesTpSlRestClient : ISharedClient
+    {
+        /// <summary>
+        /// Set take profit and/or stop loss options
+        /// </summary>
+        EndpointOptions<SetTpSlRequest> SetTpSlOptions { get; }
+        /// <summary>
+        /// Set a take profit and/or stop loss for an open position
+        /// </summary>
+        /// <param name="request">Request info</param>
+        /// <param name="ct">Cancellation token</param>
+        /// <returns></returns>
+        Task<ExchangeWebResult<SharedId>> SetTpSlAsync(SetTpSlRequest request, CancellationToken ct = default);
+
+        /// <summary>
+        /// Cancel a take profit and/or stop loss options
+        /// </summary>
+        EndpointOptions<SetTpSlRequest> CancelTpSlOptions { get; }
+        /// <summary>
+        /// Cancel an active take profit and/or stop loss for an open position
+        /// </summary>
+        /// <param name="request">Request info</param>
+        /// <param name="ct">Cancellation token</param>
+        /// <returns></returns>
+        Task<ExchangeWebResult<SharedId>> CancelTpSlAsync(CancelOrderRequest request, CancellationToken ct = default);
+    }
+}
---
+++ CryptoExchange.Net/SharedApis/Models/Rest/SetTpSlRequest.cs
@@ -0,0 +1,39 @@
+﻿using CryptoExchange.Net.Objects;
+
+namespace CryptoExchange.Net.SharedApis
+{
+    /// <summary>
+    /// Set a take profit and/or stop loss for an open position
+    /// </summary>
+    public record SetTpSlRequest : SharedSymbolRequest
+    {
+        /// <summary>
+        /// Position mode
+        /// </summary>
+        public SharedPositionMode? PositionMode { get; set; }
+        /// <summary>
+        /// Position side
+        /// </summary>
+        public SharedPositionSide PositionSide { get; set; }
+        /// <summary>
+        /// Margin mode
+        /// </summary>
+        public SharedMarginMode? MarginMode { get; set; }
+
+        public decimal? TakeProfitTriggerPrice { get; set; }
+        public decimal? TakeProfitOrderPrice { get; set; }
+        public decimal? StopLossTriggerPrice { get; set; }
+        public decimal? StopLossOrderPrice { get; set; }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="exchangeParameters">Exchange specific parameters</param>
+        public SetTpSlRequest(SharedSymbol symbol, SharedPositionSide positionSide, ExchangeParameters? exchangeParameters = null)
+            : base(symbol, exchangeParameters)
+        {
+            PositionSide = positionSide;
+            Symbol = symbol;
+        }
+    }
+}
