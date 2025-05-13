77944f03ba551cc83202c6e6c9fc80c08b8ffceb
Jkorf <jankorf91@gmail.com>
2025-05-13 08:38:54 +0200

    Added setting to disable auto conversion in AssetAliasConfiguration

--- CryptoExchange.Net/Objects/AssetAliasConfiguration.cs
+++ CryptoExchange.Net/Objects/AssetAliasConfiguration.cs
@@ -16,14 +16,19 @@ namespace CryptoExchange.Net.Objects
         public AssetAlias[] Aliases { get; set; } = [];
 
         /// <summary>
+        /// Auto convert asset names when using the Shared interfaces. Defaults to true
+        /// </summary>
+        public bool AutoConvertEnabled { get; set; } = true;
+
+        /// <summary>
         /// Map the common name to an exchange name for an asset. If there is no alias the input name is returned
         /// </summary>
-        public string CommonToExchangeName(string commonName) => Aliases.SingleOrDefault(x => x.CommonAssetName == commonName)?.ExchangeAssetName ?? commonName;
+        public string CommonToExchangeName(string commonName) => !AutoConvertEnabled ? commonName : Aliases.SingleOrDefault(x => x.CommonAssetName == commonName)?.ExchangeAssetName ?? commonName;
 
         /// <summary>
         /// Map the exchange name to a common name for an asset. If there is no alias the input name is returned
         /// </summary>
-        public string ExchangeToCommonName(string exchangeName) => Aliases.SingleOrDefault(x => x.ExchangeAssetName == exchangeName)?.CommonAssetName ?? exchangeName;
+        public string ExchangeToCommonName(string exchangeName) => !AutoConvertEnabled ? exchangeName : Aliases.SingleOrDefault(x => x.ExchangeAssetName == exchangeName)?.CommonAssetName ?? exchangeName;
 
     }
 }
