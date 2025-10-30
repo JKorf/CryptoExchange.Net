7413d03d3110e6cebe62b0901c00eb0b881e9dc1
Jkorf <jankorf91@gmail.com>
2025-10-30 11:25:27 +0100

    Added client reference helper to LibraryHelpers

--- CryptoExchange.Net/LibraryHelpers.cs
+++ CryptoExchange.Net/LibraryHelpers.cs
@@ -17,6 +17,29 @@ namespace CryptoExchange.Net
         /// </summary>
         public const string ClientOrderIdSeparator = "JK";
 
+        private static Dictionary<string, string> _defaultClientReferences = new Dictionary<string, string>()
+        {
+            { "Binance.Spot", "x-VICEW9VV" },
+            { "Binance.Futures", "x-d63tKbx3" },
+            { "BingX", "easytrading" },
+            { "Bitfinex", "kCCe-CNBO" },
+            { "Bitget", "6x21p" },
+            { "BitMart", "EASYTRADING0001" },
+            { "BitMEX", "Sent from JKorf" },
+            { "BloFin", "5c07cf695885c282" },
+            { "Bybit", "Zx000356" },
+            { "CoinEx", "x-147866029-" },
+            { "GateIo", "copytraderpw" },
+            { "HTX", "AA1ef14811" },
+            { "Kucoin.FuturesName", "Easytradingfutures" },
+            { "Kucoin.FuturesKey", "9e08c05f-454d-4580-82af-2f4c7027fd00" },
+            { "Kucoin.SpotName", "Easytrading" },
+            { "Kucoin.SpotKey", "f8ae62cb-2b3d-420c-8c98-e1c17dd4e30a" },
+            { "Mexc", "EASYT" },
+            { "OKX", "1425d83a94fbBCDE" },
+            { "XT", "4XWeqN10M1fcoI5L" },
+        };
+
         /// <summary>
         /// Apply broker id to a client order id
         /// </summary>
@@ -45,6 +68,22 @@ namespace CryptoExchange.Net
             }
 
             return clientOrderId;
+        }
+
+        /// <summary>
+        /// Get the client reference for an exchange if available
+        /// </summary>
+        public static string GetClientReference(Func<string?> optionsReference, string exchange, string? topic = null)
+        {
+            var optionsValue = optionsReference();
+            if (!string.IsNullOrEmpty(optionsValue))
+                return optionsValue!;
+
+            var key = exchange;
+            if (topic != null)
+                key += "." + topic;
+
+            return _defaultClientReferences.TryGetValue(key, out var id) ? id : throw new KeyNotFoundException($"{exchange} not found in configuration");
         }
 
         /// <summary>
