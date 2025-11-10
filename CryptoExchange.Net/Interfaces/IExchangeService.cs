9d94a24862e286bef1267d6b8f8f303e60b8fe0e
Jkorf <jankorf91@gmail.com>
2025-11-10 09:15:29 +0100

    Added IExchangeService interface

---
+++ CryptoExchange.Net/Interfaces/IExchangeService.cs
@@ -0,0 +1,13 @@
+﻿namespace CryptoExchange.Net.Interfaces
+{
+    /// <summary>
+    /// Service for an exchange
+    /// </summary>
+    public interface IExchangeService
+    {
+        /// <summary>
+        /// The exchange the service is for
+        /// </summary>
+        public string Exchange { get; }
+    }
+}
