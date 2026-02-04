3559e38146402046a8daf30c760869ca090723af
JKorf <jankorf91@gmail.com>
2026-02-04 21:46:15 +0100

    Added status to SharedDeposit model

---
+++ CryptoExchange.Net/SharedApis/Enums/SharedTransferStatus.cs
@@ -0,0 +1,21 @@
+﻿namespace CryptoExchange.Net.SharedApis
+{
+    /// <summary>
+    /// Transfer status
+    /// </summary>
+    public enum SharedTransferStatus
+    {
+        /// <summary>
+        /// In progress
+        /// </summary>
+        InProgress,
+        /// <summary>
+        /// Failed
+        /// </summary>
+        Failed,
+        /// <summary>
+        /// Completed
+        /// </summary>
+        Completed
+    }
+}
