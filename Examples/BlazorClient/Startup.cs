9ae126366235e8d6f3b342592e248a9508e119d7
Jkorf <jankorf91@gmail.com>
2026-04-10 12:57:37 +0200

    Added Weex to examples

--- Examples/BlazorClient/Startup.cs
+++ Examples/BlazorClient/Startup.cs
@@ -56,6 +56,7 @@ namespace BlazorClient
             services.AddOKX();
             services.AddToobit();
             services.AddUpbit();
+            services.AddWeex();
             services.AddWhiteBit();
             services.AddXT();
         }
