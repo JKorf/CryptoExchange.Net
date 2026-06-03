1d49eca7381e5a2afbcc8e513fd4227e6783ea5e
JKorf <jankorf91@gmail.com>
2026-06-03 21:47:41 +0200

    wip

---
+++ CryptoExchange.Net/Objects/CallResult2.cs
@@ -0,0 +1,179 @@
+﻿using CryptoExchange.Net.Objects.Sockets;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace CryptoExchange.Net.Objects;
+
+public readonly struct Unit
+{
+    public static readonly Unit Value = default;
+}
+
+public interface IOpResult
+{
+    /// <summary>
+    /// An error if the call didn't succeed, will always be filled if Success = false
+    /// </summary>
+    Error? Error { get; }
+
+    /// <summary>
+    /// Whether the call was successful
+    /// </summary>
+    bool Success { get; }
+}
+
+public class OpResult : IOpResult
+{
+    /// <summary>
+    /// Static success result
+    /// </summary>
+    public static OpResult SuccessResult { get; } = new OpResult();
+
+    /// <inheritdoc />
+    public Error? Error { get; init; }
+    /// <inheritdoc />
+#if NET5_0_OR_GREATER
+        [MemberNotNullWhen(false, nameof(Error))]
+#endif
+    public bool Success => Error == null;
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return Success ? $"Success" : $"Error: {Error}";
+    }
+}
+
+public class HttpResult<T> : IOpResult
+{
+    public HttpResult(T value, Error? error)
+    {
+        Data = value;
+        Error = error;
+    }
+
+    /// <inheritdoc />
+    public Error? Error { get; internal set; }
+    /// <inheritdoc />
+#if NET5_0_OR_GREATER
+        [MemberNotNullWhen(false, nameof(Error))]
+        [MemberNotNullWhen(true, nameof(Data))]
+#endif
+    public bool Success => Error == null;
+    /// <summary>
+    /// The original data returned by the call, only available when `OutputOriginalData` is set to `true` in the client options
+    /// </summary>
+    public string? OriginalData { get; init; }
+    /// <summary>
+    /// The data returned by the call, only available when Success = true
+    /// </summary>
+    public T? Data { get; init; }
+    /// <summary>
+    /// The request http method
+    /// </summary>
+    public HttpMethod? RequestMethod { get; init; }
+
+    /// <summary>
+    /// HTTP protocol version
+    /// </summary>
+    public Version? HttpVersion { get; init; }
+
+    /// <summary>
+    /// The headers sent with the request
+    /// </summary>
+    public HttpRequestHeaders? RequestHeaders { get; init; }
+
+    /// <summary>
+    /// The request id
+    /// </summary>
+    public int? RequestId { get; init; }
+
+    /// <summary>
+    /// The url which was requested
+    /// </summary>
+    public string? RequestUrl { get; init; }
+
+    /// <summary>
+    /// The body of the request
+    /// </summary>
+    public string? RequestBody { get; init; }
+
+    /// <summary>
+    /// Length in bytes of the response
+    /// </summary>
+    public long? ResponseLength { get; init; }
+
+    /// <summary>
+    /// The status code of the response. Note that a OK status does not always indicate success, check the Success parameter for this.
+    /// </summary>
+    public HttpStatusCode? ResponseStatusCode { get; init; }
+
+    /// <summary>
+    /// The response headers
+    /// </summary>
+    public HttpResponseHeaders? ResponseHeaders { get; init; }
+
+    /// <summary>
+    /// The time between sending the request and receiving the response
+    /// </summary>
+    public TimeSpan? ResponseTime { get; init; }
+    /// <summary>
+    /// The data source of this result
+    /// </summary>
+    public ResultDataSource DataSource { get; init; } = ResultDataSource.Server;
+}
+
+public class WebSocketResult<T> : IOpResult
+{
+    public WebSocketResult(T value, Error? error)
+    {
+        Data = value;
+        Error = error;
+    }
+
+    /// <inheritdoc />
+    public Error? Error { get; init; }
+    /// <inheritdoc />
+#if NET5_0_OR_GREATER
+        [MemberNotNullWhen(false, nameof(Error))]
+        [MemberNotNullWhen(true, nameof(Data))]
+#endif
+    public bool Success => Error == null;
+    /// <summary>
+    /// The original data returned by the call, only available when `OutputOriginalData` is set to `true` in the client options
+    /// </summary>
+    public string? OriginalData { get; init; }
+    /// <summary>
+    /// The data returned by the call, only available when Success = true
+    /// </summary>
+    public T? Data { get; init; }
+
+    /// <summary>
+    /// The request id
+    /// </summary>
+    public int? RequestId { get; init; }
+
+    /// <summary>
+    /// The url which was requested
+    /// </summary>
+    public int? ConnectionId { get; init; }
+
+    /// <summary>
+    /// The body of the request
+    /// </summary>
+    public string? RequestBody { get; init; }
+
+    /// <summary>
+    /// Length in bytes of the response
+    /// </summary>
+    public long? ResponseLength { get; init; }
+
+    /// <summary>
+    /// The time between sending the request and receiving the response
+    /// </summary>
+    public TimeSpan? ResponseTime { get; init; }
+}
--- CryptoExchange.Net/SharedApis/SharedExecutionResult.cs
+++ CryptoExchange.Net/SharedApis/SharedExecutionResult.cs
@@ -5,64 +5,70 @@ namespace CryptoExchange.Net.SharedApis
     /// <summary>
     /// Result of a Shared interface call
     /// </summary>
-    /// <typeparam name="TResult"></typeparam>
-    public readonly struct SharedExecutionResult<TResult>
+    public abstract class SharedExecutionResult
     {
         /// <summary>
         /// Error before the call was made, for example due to invalid parameters. If this is set, CallResult will be null
         /// </summary>
-        public Error? PreCallError { get; }
+        public Error? PreCallError { get; init; }
         /// <summary>
         /// The result of the web request made to the exchange
         /// </summary>
-        public IWebCallResult? CallResult { get; }
-        /// <summary>
-        /// The result data
-        /// </summary>
-        public TResult? Data { get; }
+        public IWebCallResult? CallResult { get; init; }
         /// <summary>
         /// Next page request
         /// </summary>
-        public PageRequest? NextPageRequest { get; }
+        public PageRequest? NextPageRequest { get; init; }
         /// <summary>
         /// Whether the call was successful
         /// </summary>
         public bool Success => PreCallError == null && CallResult != null && CallResult.Success;
-
-        private SharedExecutionResult(Error error)
-        {
-            PreCallError = error;
-        }
-
-        private SharedExecutionResult(IWebCallResult callResult, TResult? data, PageRequest? nextPageRequest)
-        {
-            CallResult = callResult;
-            Data = data;
-            NextPageRequest = nextPageRequest;
-        }
 
         /// <summary>
         /// Create a success result
         /// </summary>
         /// <param name="callResult">Call result</param>
         /// <param name="data">Result data</param>
-        public static SharedExecutionResult<TResult> Ok(IWebCallResult callResult, TResult data) => new SharedExecutionResult<TResult>(callResult, data, null);
+        public static SharedExecutionResult<TResult> Ok<TResult>(IWebCallResult callResult, TResult data) => new SharedExecutionResult<TResult>(callResult, data, null);
         /// <summary>
         /// Create a success result
         /// </summary>
         /// <param name="callResult">Call result</param>
         /// <param name="data">Result data</param>
         /// <param name="nextPageRequest">Pagination token</param>
-        public static SharedExecutionResult<TResult> Ok(IWebCallResult callResult, TResult data, PageRequest? nextPageRequest) => new SharedExecutionResult<TResult>(callResult, data, nextPageRequest);
+        public static SharedExecutionResult<TResult> Ok<TResult>(IWebCallResult callResult, TResult data, PageRequest? nextPageRequest) => new SharedExecutionResult<TResult>(callResult, data, nextPageRequest);
         /// <summary>
         /// Create an error result
         /// </summary>
         /// <param name="callResult">The failed result</param>
-        public static SharedExecutionResult<TResult> Error(IWebCallResult callResult) => new SharedExecutionResult<TResult>(callResult, default, default);
+        public static SharedExecutionResult<TResult> Error<TResult>(IWebCallResult callResult) => new SharedExecutionResult<TResult>(callResult, default, default);
         /// <summary>
         /// Create an error result
         /// </summary>
         /// <param name="error">The pre-call result</param>
-        public static SharedExecutionResult<TResult> Error(Error error) => new SharedExecutionResult<TResult>(error);
+        public static SharedExecutionResult<TResult> Error<TResult>(Error error) => new SharedExecutionResult<TResult>(error);
+    }
+
+    /// <summary>
+    /// Result of a Shared interface call
+    /// </summary>
+    public class SharedExecutionResult<TResult> : SharedExecutionResult
+    {        
+        /// <summary>
+        /// The result data
+        /// </summary>
+        public TResult? Data { get; }
+
+        internal SharedExecutionResult(Error error)
+        {
+            PreCallError = error;
+        }
+
+        internal SharedExecutionResult(IWebCallResult callResult, TResult? data, PageRequest? nextPageRequest)
+        {
+            CallResult = callResult;
+            Data = data;
+            NextPageRequest = nextPageRequest;
+        }
     }
 }
